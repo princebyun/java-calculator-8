e78e1529f018211e12166ef90fea86b4d098bee6
princebyun <princebyun@gmail.com>
2025-10-17 00:19:29 +0900

    feat : 사용자 문자열 받는 기능
    - 사용자 입력 메서드 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    static String userInputValue = ""; //유저 입력 변수
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        userInput();
     }
+
+    /**
+     * 문자열 받는 기능
+     */
+    public static void userInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        userInputValue = readLine();
+        System.out.println("입력받은 값 : " + userInputValue);
+    }
+
 }
